1801937b7278ec0ba0a4d06ddb6b046bcf9ef927
Max Eckel <maxeckel@users.noreply.github.com>
2021-01-03 16:23:39 +0000

    Apply fixes from StyleCI

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,16 @@
 
 namespace Maxeckel\LivewireEditorjs\Tests;
 
-use Orchestra\Testbench\TestCase;
 use Maxeckel\LivewireEditorjs\LivewireEditorjsServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [LivewireEditorjsServiceProvider::class];
     }
-    
+
     /** @test */
     public function true_is_true()
     {
